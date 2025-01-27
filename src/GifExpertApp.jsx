04a4a04bbb21e27074d995b2f1c3eed1445bd121
Diego Madrid <analista8@mic.com.co>
2025-01-27 14:53:50 -0500

    WE FINISHES THE PROJECT

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -5,28 +5,20 @@ import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
 
-    const [categories, setCategories] = useState(["dragon ball"]);
+    const [categories, setCategories] = useState([]);
 
     const onAddCategory = (newCategory) => {
         if (categories.find(category => category.toLowerCase() === newCategory.toLowerCase())) return;
-
         setCategories([newCategory.toLowerCase(), ...categories]);
     }
-
-    
-
     return (
         <>
             <h1>GifExpertApp</h1>
-            <AddCategory
-                onNewCategory={value => onAddCategory(value)}
-            />
+            <AddCategory onNewCategory={value => onAddCategory(value)}/>
             {
                 categories.map(category => <GifGrid category={category} key={category} />
                 )}
         </>
-
-
     )
 }
 
